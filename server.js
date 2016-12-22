a343ed49be8aac5a79e5add3e6f7e97c81381612
Eric Castoldi <ericgtr17@gmail.com>
2016-12-22 13:02:43 -0200

    Montagem do Site

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
+var path = require('path');
 var express = require('express');
 
+var app = express();
 app.use(express.static(path.join(__dirname, '/')));
 app.use(express.static(path.join(__dirname, '/img')));
+
+app.listen(80, function() {
+  console.log('Server up and running! http://localhost:80/');
+});
